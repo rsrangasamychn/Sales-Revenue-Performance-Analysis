f2f065bd97287e81b97845765832bd2f6a3b8daf
Rangasamy S <137636265+rsrangasamychn@users.noreply.github.com>
2025-12-12 12:34:23 +0530

    Add files via upload

---
+++ Sales-Revenue-Performance-Analysis/data/config.py
@@ -0,0 +1,22 @@
+import os
+from dotenv import load_dotenv
+from sqlalchemy import create_engine, event
+
+load_dotenv(dotenv_path = "C:/Users/hanav/Data Analysis/.env")
+
+user = os.getenv("DB_USER")
+password = os.getenv("DB_PASS")
+host = os.getenv("DB_HOST")
+db = os.getenv("DB_NAME")
+
+engine = create_engine(
+    f"mysql+mysqlconnector://{user}:{password}@{host}/{db}"
+)
+
+@event.listens_for(engine, "connect")
+def set_readonly(dbapi_conn, connection_record):
+    cursor = dbapi_conn.cursor()
+    cursor.execute("SET SESSION TRANSACTION READ ONLY;")
+    cursor.close()
+
+print("Connected in READ-ONLY mode!")
